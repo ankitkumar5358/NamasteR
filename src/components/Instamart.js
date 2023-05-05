7d00e11888f89d3f98468d753f8813ed6c070bab
Ankit Kumar <57992836+ankitkumar5358@users.noreply.github.com>
2023-05-05 23:35:14 +0530

    isntamart section

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+  // const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div className='text-center border border-black py-2 px-2'>
+      <h3>{title}</h3>
+      {isVisible ?
+        <button onClick={() => setIsVisible(false)} className="rounded hover:rounded-lg">Hide</button>
+        :
+        <button onClick={() => setIsVisible(true)} className="rounded hover:rounded-lg">Show</button>
+      }
+      {isVisible && <p> {description} </p>}
+    </div>
+  )
+}
 
 const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({
+    showAbout: true,
+    shopwTeam: false,
+    showCareer: true
+  })
   return (
-    <>
-    <h1>Instamart</h1>
-    <h1>100s of component inside it</h1>
-    </>
+    <div>
+      <h1 className='text-3xl p-2 m-2 font-bold'> Instamart </h1>
+      <Section
+        title={"About Instamart"}
+        description={"Everything you need gets delivered in minutes with Swiggy Instamart. Order now. Order fruits, veggies, milk, munchies & more online from Swiggy Instamart. Great deals. Safe packing. Door Step Delivery. Instant delivery. Fresh Products. "}
+        isVisible={sectionConfig.showAbout}
+        setIsVisible={() => {
+          setSectionConfig({
+            showAbout: true,
+            shopwTeam: false,
+            showCareer: false
+          })
+        }}
+      />
+      <Section
+        title={"Team Instamart"}
+        description={"This is team  instamart page. Team has 100+ members. Everything you need gets delivered in minutes with Swiggy Instamart. Order now. Order fruits, veggies, milk, munchies & more online from Swiggy Instamart. Great deals. Safe packing. Door Step Delivery. Instant delivery. Fresh Products."}
+        isVisible={sectionConfig.shopwTeam}
+        setIsVisible={() => {
+          setSectionConfig({
+            showAbout: false,
+            shopwTeam: true,
+            showCareer: false
+          })
+        }}
+      />
+      <Section
+        title={"Career Instamart"}
+        description={"This is career  instamart page. Team has 100+ members. Everything you need gets delivered in minutes with Swiggy Instamart. Order now. Order fruits, veggies, milk, munchies & more online from Swiggy Instamart. Great deals. Safe packing. Door Step Delivery. Instant delivery. Fresh Products."}
+        isVisible={sectionConfig.showCareer}
+        setIsVisible={() => {
+          setSectionConfig({
+            showAbout: false,
+            shopwTeam: false,
+            showCareer: true
+          })
+        }}
+      />
+      {/* <AboutInstaMart/>
+    <DetailsOfInstaMart/>
+    <TeamInstaMart/>
+    <Product/>
+    <Careers/>  */}
+    </div>
   );
 };
 
