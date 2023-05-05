5617cca7564097f5c938cd00cb3261a3be4f37b2
Ankit Kumar <57992836+ankitkumar5358@users.noreply.github.com>
2023-05-05 21:52:15 +0530

    shimmer ui in tailwind code

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,20 +1,15 @@
-
 const Shimmer = () => {
   return (
-    <section className="restaurant-list">
+    <section className="bg-gray-100">
       {Array(10).fill().map((_, index) => (
-        <article key={index} className="restaurant-card">
+        <article key={index} className="bg-white rounded-md p-4 my-4 shadow-md">
           {/* Shimmer effect */}
-          <div className="shimmer-wrapper">
-            <div className="shimmer-line shimmer-line-1"></div>
-            <div className="shimmer-line shimmer-line-2"></div>
-            <div className="shimmer-line shimmer-line-3"></div>
-          </div>
+          <div className="bg-gray-200 h-44 rounded-md animate-pulse mb-4"></div>
           {/* Card content */}
           <div className="card-content">
-            <div className="card-title"></div>
-            <div className="card-text"></div>
-            <div className="card-text"></div>
+            <div className="card-title bg-gray-200 h-6 rounded-md animate-pulse w-3/4 mb-2"></div>
+            <div className="card-text bg-gray-200 h-4 rounded-md animate-pulse w-2/4 mb-2"></div>
+            <div className="card-text bg-gray-200 h-4 rounded-md animate-pulse w-5/4 mb-2"></div>
           </div>
         </article>
       ))}
