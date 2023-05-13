bf0419228b55c1fca790d016c5fdc01d83b33ff3
Ankit Kumar <57992836+ankitkumar5358@users.noreply.github.com>
2023-05-13 23:52:19 +0530

    cart and fooditem component commit, 13th may

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,6 +4,43 @@ import FoodItem from "./FoodItem";
 import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
+  const cartItems = useSelector((store) => store.cart.items);
+
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  return (
+    <>
+      <div className="flex">
+        <h1 className="font-bold text-xl p-2 m-2">
+          Cart Items: {cartItems.length}
+        </h1>
+        {cartItems.length !== 0 && (
+          <button
+            className=" bg-orange-400 mx-5 hover:shadow-md rounded-md p-2"
+            onClick={() => handleClearCart()}
+          >
+            Clear cart
+          </button>
+        )}
+      </div>
+      {cartItems.length === 0 ? (
+        <h1 className="font-bold text-xl p-2 m-2">Your cart is empty! </h1>
+      ) : (
+        <div className="flex flex-wrap">
+          {cartItems.map((items, index) => (
+            <FoodItem {...items} index={index} key={items.id} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+/*
+const Cart = () => {
     const cartItems = useSelector((store) => store.cart.items);
   
     const dispatch = useDispatch();
@@ -38,6 +75,6 @@ const Cart = () => {
       </>
     );
   };
-  
+*/  
 
 export default Cart;
--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,23 +1,24 @@
-import { IMG_URL} from "../constants";
+import { IMG_CDN_URL} from "../constants";
 import { clearCart, removeItem } from "../utils/cartSlice";
 import { useDispatch } from "react-redux";
 
-const FoodItem = ({ name, costForTwo, imageId, id, description, index }) => {
+const FoodItem = ({ name, price, imageId, id, description, index  }) => {
   const dispatch = useDispatch();
   const handleRemoveItem = (index) => {
     dispatch(clearCart(index));
   };
 
-  console.log(index);
+  // console.log(index);
   return (
     <div key={id} className="w-56 m-2 p-2 bg-orange-100 shadow-md">
-      <img className="p-2" src={IMG_URL + id} />
+      <img className="p-2" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" + imageId} />
       <h2 className="font-bold text-md">{name}</h2>
-      <h2 className="bg-orange-300 font-semibold">price: {costForTwo / 100} rs.</h2>
+      <h2 className="bg-orange-300 font-semibold">price: {price / 100} rs.</h2>
+      <p className="bg-orange-300 font-semibold"> {description}</p>
       <button onClick={() => handleRemoveItem()}>
         Delete
       </button>
-      <h3>{index}</h3>
+      {/* <h3>{index}</h3> */}
     </div>
   );
 };
